bfb881d9ce7fb210a84c68e720591721e2c66f23
Truong Tommy <truongtommy1999@gmail.com>
2022-07-07 09:22:03 +0700

    Updated folder

---
+++ NFCScanHoKhau/NFCScanHoKhau/NFCScanHoKhauApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCScanHoKhauApp.swift
+//  NFCScanHoKhau
+//
+//  Created by Truong Tommy on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NFCScanHoKhauApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
